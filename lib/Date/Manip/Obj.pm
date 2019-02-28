96ba6229e6dbe4daab415f3626c1c9176a93122b
Sullivan Beck <sbeck@cpan.org>
2019-02-28 13:18:01 -0500

    Release: 6.76

--- lib/Date/Manip/Obj.pm
+++ lib/Date/Manip/Obj.pm
@@ -1,5 +1,5 @@
 package Date::Manip::Obj;
-# Copyright (c) 2008-2018 Sullivan Beck. All rights reserved.
+# Copyright (c) 2008-2019 Sullivan Beck. All rights reserved.
 # This program is free software; you can redistribute it and/or modify it
 # under the same terms as Perl itself.
 
